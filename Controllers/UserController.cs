99b26723ecd08fbe003d7996028e032bd87be7f1
mkheck13 <mkheck13@gmail.com>
2025-03-24 10:58:54 -0700

    day 3 lecture

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -20,13 +20,33 @@ namespace blog.Controllers
         }
 
         [HttpPost("CreateUser")]
-        public async Task<IActionResult> CreateUser(UserDTO user)
+        public async Task<IActionResult> CreateUser([FromBody]UserDTO user)
         {
             bool success = await _userServices.CreateUser(user);
 
             if(success) return Ok(new {Success = true});
 
             return BadRequest(new {Success = false, Message = "User already Exists"});
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody]UserDTO user)
+        {
+            var success = await _userServices.Login(user);
+
+            if(success != null) return Ok(new {Token = success});
+
+            return Unauthorized(new {Message = "Username or Password Doesn't Match our system"});
+        }
+
+        [HttpGet("GetUserInfoByUsername/{username}")]
+        public async Task<IActionResult> GetUserInfoByUsername(string username)
+        {
+            var user = await _userServices.GetUserInfoByUserName(username);
+
+            if(user != null) return Ok(user);
+
+            return BadRequest(new {Message = "No User Found"});
         }
     }
 }
